3875b338ffcd17aceda01deaf1772f331effc14b
Grauly <nils.dellemann@web.de>
2022-08-21 18:23:54 +0200

    feat: boss bars now move down to not intersect with the compass

---
+++ src/main/java/grauly/hudcompass/mixin/BossBarHudMixin.java
@@ -0,0 +1,22 @@
+package grauly.hudcompass.mixin;
+
+import net.minecraft.client.gui.hud.BossBarHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BossBarHud.class)
+public abstract class BossBarHudMixin {
+
+    @Inject(at = @At("HEAD"), method = "render")
+    public void renderMoveDown(MatrixStack matrices, CallbackInfo ci) {
+        matrices.translate(0,30,0);
+    }
+
+    @Inject(at = @At("TAIL"), method = "render")
+    public void renderResetMove(MatrixStack matrices, CallbackInfo ci) {
+        matrices.translate(0,-30,0);
+    }
+}
