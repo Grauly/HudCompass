fc01bcb4fab15fe434f4f0e17429b0cb187f1cd1
Grauly <nils.dellemann@web.de>
2024-08-05 23:44:57 +0200

    fix: "replace" id rendering

--- src/main/java/grauly/hudcompass/util/RendererHelper.java
+++ src/main/java/grauly/hudcompass/util/RendererHelper.java
@@ -3,16 +3,56 @@ package grauly.hudcompass.util;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+
 public class RendererHelper {
-    private static final Identifier MAP_ICONS = Identifier.of("minecraft", "textures/map/map_icons.png");
+    public static final ArrayList<Identifier> MAP_ICONS = new ArrayList<>();
+
+    private static void addIcon(String id) {
+        MAP_ICONS.add(Identifier.ofVanilla("textures/map/decorations/" + id + ".png"));
+    }
+
+    static {
+        addIcon("player");
+        addIcon("frame");
+        addIcon("red_marker");
+        addIcon("blue_marker");
+        addIcon("target_x");
+        addIcon("target_point");
+        addIcon("player_off_map");
+        addIcon("player_off_limits");
+        addIcon("woodland_mansion");
+        addIcon("ocean_monument");
+        addIcon("white_banner");
+        addIcon("orange_banner");
+        addIcon("magenta_banner");
+        addIcon("light_blue_banner");
+        addIcon("yellow_banner");
+        addIcon("lime_banner");
+        addIcon("pink_banner");
+        addIcon("gray_banner");
+        addIcon("light_gray_banner");
+        addIcon("cyan_banner");
+        addIcon("purple_banner");
+        addIcon("blue_banner");
+        addIcon("brown_banner");
+        addIcon("green_banner");
+        addIcon("red_banner");
+        addIcon("black_banner");
+        addIcon("red_x");
+        addIcon("desert_village");
+        addIcon("plains_village");
+        addIcon("savanna_village");
+        addIcon("snowy_village");
+        addIcon("taiga_village");
+        addIcon("jungle_temple");
+        addIcon("swamp_hut");
+        addIcon("trial_chambers");
+    }
 
     public static void drawWaypointIcon(DrawContext context, int centerX, int floorY, int id) {
-        var yOffset = 0;
-        while (id >= 16) {
-            id -= 16;
-            yOffset += 8;
-        }
-        context.drawTexture(MAP_ICONS, centerX - 4, floorY - 8, 0, id * 8, yOffset, 8, 8, 128, 128);
+        Identifier drawTarget = id > MAP_ICONS.size() ? Identifier.of("void") : MAP_ICONS.get(id);
+        context.drawTexture(drawTarget, centerX - 4, floorY - 8, 0, 0, 0, 8, 8, 8, 8);
     }
 
     public static void drawScaledWaypointIcon(DrawContext context, int centerX, int floorY, int id, int scale) {
