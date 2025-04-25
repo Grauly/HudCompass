5a5f3626a78dce0d26917964621c6c5a0721205f
Grauly <nils.dellemann@web.de>
2025-04-25 20:41:05 +0200

    fix: move to layer hud callback

--- src/main/java/grauly/hudcompass/HudCompassClient.java
+++ src/main/java/grauly/hudcompass/HudCompassClient.java
@@ -7,20 +7,26 @@ import grauly.hudcompass.waypoints.WaypointManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class HudCompassClient implements ClientModInitializer {
 
     public static final WaypointManager waypointManager = new WaypointManager();
     public static final MinecraftClient mc = MinecraftClient.getInstance();
+    public static final Identifier COMPASS_LAYER = Identifier.of(HudCompass.MODID, "compass");
 
     @Override
     public void onInitializeClient() {
-        HudRenderCallback.EVENT.register(HUDCompassRenderer::onRender);
+        HudLayerRegistrationCallback.EVENT.register(layeredDrawerWrapper -> {
+            layeredDrawerWrapper.attachLayerAfter(IdentifiedLayer.SUBTITLES, COMPASS_LAYER, HUDCompassRenderer::onRender);
+        });
         var newWaypointKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.hudcompass.newpoint",
                 InputUtil.Type.KEYSYM,
