abb8bcd1048243f50ade59e9b2314a7dc2f65795
Grauly <nils.dellemann@web.de>
2025-09-28 23:04:11 +0200

    feat: add icon loading capability

--- src/main/java/grauly/hudcompass/HudCompassClient.java
+++ src/main/java/grauly/hudcompass/HudCompassClient.java
@@ -1,6 +1,7 @@
 package grauly.hudcompass;
 
 import grauly.hudcompass.rendering.HUDCompassRenderer;
+import grauly.hudcompass.resources.IconStore;
 import grauly.hudcompass.screens.ConfigureWaypointScreen;
 import grauly.hudcompass.screens.WaypointListScreen;
 import grauly.hudcompass.waypoints.WaypointManager;
@@ -9,9 +10,11 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
+import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
@@ -19,6 +22,7 @@ public class HudCompassClient implements ClientModInitializer {
 
     public static final WaypointManager waypointManager = new WaypointManager();
     public static final MinecraftClient mc = MinecraftClient.getInstance();
+    public static final IconStore ICON_STORE = new IconStore();
     public static final Identifier COMPASS_LAYER = Identifier.of(HudCompass.MODID, "compass");
 
     @Override
@@ -45,5 +49,9 @@ public class HudCompassClient implements ClientModInitializer {
                 mc.setScreen(new WaypointListScreen(mc.currentScreen));
             }
         });
+        ResourceLoader.get(ResourceType.CLIENT_RESOURCES).registerReloader(
+                Identifier.of(HudCompass.MODID, "icons"),
+                ICON_STORE
+        );
     }
 }
