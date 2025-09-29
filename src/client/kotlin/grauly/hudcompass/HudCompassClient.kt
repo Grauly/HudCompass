c7f23a5452aa55a4d652f5d8124ac119e42a71e0
Grauly <nils.dellemann@web.de>
2025-09-29 18:01:40 +0200

    partial: first start on moving to kotlin rewrite

---
+++ src/client/kotlin/grauly/hudcompass/HudCompassClient.kt
@@ -0,0 +1,9 @@
+package grauly.hudcompass
+
+import net.fabricmc.api.ClientModInitializer
+
+object HudCompassClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/grauly/hudcompass/HudCompassDataGenerator.kt
@@ -0,0 +1,9 @@
+package grauly.hudcompass
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object HudCompassDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
