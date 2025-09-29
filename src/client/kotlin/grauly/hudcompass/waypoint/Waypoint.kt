cea0c6f191caccbf82e606e9ded3e082d8026c6c
Grauly <nils.dellemann@web.de>
2025-09-30 00:04:10 +0200

    fix: generalized dimensionId check

--- src/client/kotlin/grauly/hudcompass/waypoint/Waypoint.kt
+++ src/client/kotlin/grauly/hudcompass/waypoint/Waypoint.kt
@@ -7,7 +7,7 @@ import net.minecraft.util.Identifier
 
 interface Waypoint {
     fun getLocation(): WaypointLocation
-    fun getDimensionId(): Identifier
+    fun shouldShow(dimensionId: Identifier): Boolean
     fun getIconRenderer(): WaypointIconRenderer
 
     fun toRenderState(): WaypointRenderState = WaypointRenderState(getLocation(), getIconRenderer())
