3347c9bd5949c30a02e5d0b2f721994222f8200e
Grauly <nils.dellemann@web.de>
2025-09-29 18:11:04 +0200

    feat: setup datagen and more stubs

--- src/client/kotlin/grauly/hudcompass/HudCompassClient.kt
+++ src/client/kotlin/grauly/hudcompass/HudCompassClient.kt
@@ -1,8 +1,11 @@
 package grauly.hudcompass
 
 import net.fabricmc.api.ClientModInitializer
+import org.slf4j.LoggerFactory
 
 object HudCompassClient : ClientModInitializer {
+	const val MODID = "hudcompass"
+	val logger = LoggerFactory.getLogger(MODID)
 	override fun onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
---
+++ src/datagen/kotlin/grauly/hudcompass/HudCompassDatagen.kt
@@ -0,0 +1,14 @@
+package grauly.hudcompass
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object HudCompassDatagen: DataGeneratorEntrypoint {
+    override fun onInitializeDataGenerator(p0: FabricDataGenerator?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getEffectiveModId(): String? {
+        return super.getEffectiveModId()
+    }
+}
