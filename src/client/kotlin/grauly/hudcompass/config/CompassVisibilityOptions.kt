457ed20462f891907d4085b527fa12b374e1a49c
Grauly <nils.dellemann@web.de>
2025-10-01 21:39:21 +0200

    fix: add toggle behavior to config

--- src/client/kotlin/grauly/hudcompass/config/CompassVisibilityOptions.kt
+++ src/client/kotlin/grauly/hudcompass/config/CompassVisibilityOptions.kt
@@ -2,6 +2,8 @@ package grauly.hudcompass.config
 
 enum class CompassVisibilityOptions {
     ALWAYS,
+    TOGGLE_WAYPOINTS,
+    TOGGLE_COMPASS,
     HOTKEY_WAYPOINTS,
     HOTKEY_COMPASS,
     NEVER
