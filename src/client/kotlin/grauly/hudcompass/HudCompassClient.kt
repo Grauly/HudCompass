139eaf6264d91e81dcd43fc1631cc0283b1364c9
Grauly <nils.dellemann@web.de>
2025-10-01 22:11:41 +0200

    feat: add config

--- src/client/kotlin/grauly/hudcompass/HudCompassClient.kt
+++ src/client/kotlin/grauly/hudcompass/HudCompassClient.kt
@@ -1,8 +1,11 @@
 package grauly.hudcompass
 
+import folk.sisby.kaleido.api.WrappedConfig
+import grauly.hudcompass.config.HudCompassConfig
 import grauly.hudcompass.waypoint.WaypointStore
 import grauly.hudcompass.waypoint.provider.HudCompassWaypointProvider
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.loader.api.FabricLoader
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -10,6 +13,12 @@ object HudCompassClient : ClientModInitializer {
     const val MODID = "hudcompass"
     val logger: Logger = LoggerFactory.getLogger(MODID)
 	val hudCompassWaypoints: HudCompassWaypointProvider = HudCompassWaypointProvider()
+    val config = WrappedConfig.createToml(
+    FabricLoader.getInstance().configDir,
+        MODID,
+        "main",
+        HudCompassConfig::class.java
+    )
 
     override fun onInitializeClient() {
         KeyBindings.init()
